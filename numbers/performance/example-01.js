74ad817b869c437dc64444a6224bf0631069b4e3
Daniel Santana <daniel@everymundo.com>
2022-06-20 10:19:22 -0400

    add smoe other tests

---
+++ numbers/performance/example-01.js
@@ -0,0 +1,25 @@
+const doSomeLongRunningProcess = (cb) => {
+  for (let i = 10 ** 10; i-- < 0;) {
+    i.toString()
+  }
+
+  cb()
+}
+const { PerformanceObserver, performance } = require('perf_hooks')
+
+const obs = new PerformanceObserver((items) => {
+  console.log(items.getEntries()[0].duration)
+
+  performance.clearMarks()
+})
+
+obs.observe({ entryTypes: ['measure'] })
+performance.measure('Start to Now')
+
+performance.mark('A')
+doSomeLongRunningProcess(() => {
+  performance.measure('A to Now', 'A')
+
+  performance.mark('B')
+  performance.measure('A to B', 'A', 'B')
+})
--- string/regexp-replace--vs--trim.benchmark.js
+++ string/regexp-replace--vs--trim.benchmark.js
@@ -5,20 +5,22 @@
 /* eslint-disable import/no-extraneous-dependencies, no-console */
 const assert = require('assert')
 
-const suite = require('../lib/bench')
+// const suite = require('../lib/bench')
+const { suite } = require('../lib/perf')
 
 // eslint-disable-next-line func-names
 
 const replace = (s) => ('' + s).replace(/^\s+/, '').replace(/\s+$/, '')
 const trim = (s) => ('' + s).trim()
+const doubleTrim = (s) => ('' + s).trimStart(' ').trimEnd(' ')
 
 assert(replace(' \n  a  \t  ') === 'a', 'replace fail the test')
 assert(trim(' \n  a  \t  ') === 'a', 'staticSubstrConcat fail the test')
 
 // add tests
 suite
-  .add('trim    ', () => trim('  a   '))
-  .add('replace ', () => replace('  a   '))
-  .run({
-    async: true
-  })
+  .add('trim      ', () => trim('  a   '))
+  .add('replace   ', () => replace('  a   '))
+  .add('doubleTrim', () => doubleTrim('  a   '))
+  // .run({ async: false, runForXms: 2000 })
+  .simpleSync(2000)
